8bf8bde8fa9062764e3e9535df21bac8978ac567
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-14 01:08:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,20 +9,28 @@ class Student
  #this will use #send. also, add the newly created student to the @@all array.
     student_hash.each do
       |key, value| self.send(("#{key}="), value)
-      binding.pry
     end
     @@all << self
   end
 
-  def self.create_from_collection(student)
+  def self.create_from_collection(students_array)
     #iterates over the array of hashes and crates a new individaul student using each hash.
     #argument is: the return value of the scraper.scrape_index_page
-    students_array
+    students_array.each do |student_hash|
+      Student.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-
+    attributes_hash do |key, value| 
+      self.send(("#{key}="), value)
+    end
+    self
   end
+
+  def new
+
+  end 
 
   def self.all
       @@all
